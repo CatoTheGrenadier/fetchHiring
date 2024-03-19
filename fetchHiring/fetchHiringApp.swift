c9452eeaea9bf0e12fbda7ece8909a677a95f9c1
Cato the Chevalier <131333594+CatoTheGrenadier@users.noreply.github.com>
2024-03-19 18:08:34 -0400

    Initial Commit

---
+++ fetchHiring/fetchHiringApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchHiringApp.swift
+//  fetchHiring
+//
+//  Created by Yi Ling on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct fetchHiringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
